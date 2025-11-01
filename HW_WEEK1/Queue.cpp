4267a094003bcdc1b99942c867c863d23c2f9bf1
nadia13001 <nadialosev1@gmail.com>
2025-11-01 14:45:08 +0200

    final changes and documentation

--- HW_WEEK1/Queue.cpp
+++ HW_WEEK1/Queue.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "Queue.h"
 /*
-
+Function puts initial values into queue struct and allocates memory for the array
+Input: queue struct, size of the queue
+Output: none
 */
 void initQueue(Queue* q, unsigned int size)
 {
@@ -12,7 +14,9 @@ void initQueue(Queue* q, unsigned int size)
 	q->sizeUsed = 0;
 }
 /*
-
+Function releases dynamically allocated memory and resets queue fields
+Input: queue struct
+Output: none
 */
 void cleanQueue(Queue* q)
 {
@@ -24,50 +28,60 @@ void cleanQueue(Queue* q)
 	q->sizeUsed = 0;
 }
 /*
-
+Function inserts a new value into the queue if it's not full
+Input: queue struct, value to insert
+Output: none
 */
 void enqueue(Queue* q, unsigned int newValue)
 {
-	if (isFull(q))
+	if (!isFull(q))
 	{
-		return;
+		q->arr[q->last] = newValue;
+		q->last++;
+		if (q->last == q->size)
+		{
+			q->last = 0;
+		}
+		q->sizeUsed++;
 	}
-	q->arr[q->last] = newValue;
-	q->last++;
-	if (q->last == q->size)
-	{
-		q->last = 0;
-	}
-	q->sizeUsed++;
 }
 /*
-
+Function removes and returns the first value from the queue if it's not empty
+Input: queue struct
+Output: int value removed from queue, or -1 if queue is empty
 */
 int dequeue(Queue* q)
 {
 	int value = 0;
 	if (isEmpty(q))
 	{
-		return -1;
+		value = -1;
 	}
-	value = q->arr[q->first];
-	q->first++;
-	if (q->first == q->size)
+	else
 	{
-		q->first = 0;
+		value = q->arr[q->first];
+		q->first++;
+		if (q->first == q->size)
+		{
+			q->first = 0;
+		}
+		q->sizeUsed--;
 	}
-	q->sizeUsed--;
 	return value;
 }
 /*
-
+Function checks if the queue is empty
+Input: queue struct
+Output: true if empty, false otherwise
 */
 bool isEmpty(Queue* s)
 {
 	return s->sizeUsed == 0;
 }
 /*
-
+Function checks if the queue is full
+Input: queue struct
+Output: true if full, false otherwise
 */
 bool isFull(Queue* s)
 {
