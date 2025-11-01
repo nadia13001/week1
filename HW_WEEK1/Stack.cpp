fbb60ee950930a324a6327b8869e2216f20f2159
nadia13001 <nadialosev1@gmail.com>
2025-11-01 20:18:33 +0200

    add documandation

--- HW_WEEK1/Stack.cpp
+++ HW_WEEK1/Stack.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include "Stack.h"
 
+/*
+Func adds a new element to the top of the stack.
+Input: pointer to a Stack. element to add 
+Output: none
+*/
 void push(Stack* s, unsigned int element)
 {
 	addToList(&(s->stackTop),element);
 }
-
+/*
+Removes the top element from the stack and returns its value.
+Input: pointer to a Stack.
+Output: int value of the popped element or -1 if stack is empty.
+*/
 int pop(Stack* s)
 {
 	int value = 0;
@@ -20,22 +29,34 @@ int pop(Stack* s)
 	}
 	return value;
 }
-
+/*
+Func initializes the stack
+Input: pointer to a Stack
+Output: none
+*/
 void initStack(Stack* s)
 {
 	s->stackTop = nullptr;
 }
-
+/*
+Func frees all elements in the stack and resets its top pointer
+Input: pointer to a Stack
+Output: none
+*/
 void cleanStack(Stack* s)
 {
 	freeList(&(s->stackTop));
 }
-
+/*
+Checks whether the stack is empty.
+Input: pointer to a Stack.
+Output: true if the stack is empty, false otherwise
+*/
 bool isEmpty(Stack* s)
 {
 	return s ->stackTop == nullptr;
 }
-
+// There is no maximum cartridge size.
 bool isFull(Stack* s)
 {
 	return false;
