d59fd8cc220d9e50fa4f0cb6d6a5ebe09bec6f23
nadia13001 <nadialosev1@gmail.com>
2025-11-01 21:35:17 +0200

    func reverse10 in utils.cpp

--- HW_WEEK1/Utils.cpp
+++ HW_WEEK1/Utils.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Utils.h"
 #include "Stack.h"
+#define ARR_SIZE 10
 /*
 Reverses the order of elements in an integer array using a stack.
 Input: pointer to array, the number of elements in the array.
@@ -22,9 +23,23 @@ void reverse(int* nums, unsigned int size)
 	cleanStack(&s);
 }
 /*
-
+Reads 10 integers from the user stores them in array,and reverse the order using a stack.
+Input: none.
+Output: pointer to a dynamically allocated array of 10 integers in reverse order.
 */
 int* reverse10()
 {
-
+	int i = 0;
+	int value = 0;
+	int* arr = new int[ARR_SIZE];
+	Stack s;
+	initStack(&s);
+	std::cout << "Please enter 10 nums:" << std::endl;
+	for (i = 0; i < ARR_SIZE; i++)
+	{
+		std::cin >> value;
+		arr[i] = value;
+	}
+	reverse(arr, ARR_SIZE);
+	return arr;
 }
