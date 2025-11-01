23cd43e1f38f21ac85158d7f5d6fb0a3a12abe0a
nadia13001 <nadialosev1@gmail.com>
2025-11-01 02:00:36 +0200

    Adding Queue.h and starting Queue.cpp

---
+++ HW_WEEK1/Queue.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "Queue.h"
+/*
+
+*/
+void initQueue(Queue* q, unsigned int size)
+{
+
+}
+/*
+
+*/
+void cleanQueue(Queue* q)
+{
+
+}
+/*
+
+*/
+void enqueue(Queue* q, unsigned int newValue)
+{
+
+}
+/*
+
+*/
+int dequeue(Queue* q)
+{
+
+}
+/*
+
+*/
+bool isEmpty(Queue* s)
+{
+
+}
+/*
+
+*/
+bool isFull(Queue* s)
+{
+
+}
